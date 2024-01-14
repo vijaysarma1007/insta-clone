b126a867d38d25f4879e274104d53177e93a6969
vijaysarma1007 <svmsarma53@gmail.com>
2024-01-14 11:40:14 +0530

    added posts component and loaded with dummy data

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 export default function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img src={userImg} alt={username} className="h-12 rounded-full object-cover border p-1 mr-3" />
+        <p className="font-bold flex-1">{username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
+
+      {/* Post image */}
+      <img src={img} alt={`Picture of ${caption}`} className="object-cover w-full" />
     </div>
   );
 }
